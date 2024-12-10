212a84fead0a2c7fca5d138ab245e99670fcad5e
Sowparna Shome <soupornoshome@gmail.com>
2024-12-10 22:10:15 +0530

    application db context create

---
+++ Sticker-web/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Sticker_web.Models;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+
+namespace Sticker_web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        //public DbSet<Vendor> Vendors { get; set; }
+        //public DbSet<Courier> Couriers { get; set; }
+        //public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+        //public DbSet<OrderHeader> OrderHeaders { get; set; }
+        //public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
