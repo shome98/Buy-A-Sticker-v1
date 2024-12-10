f26afb0ac2537e72c271771e9293283bd4b94445
Sowparna Shome <soupornoshome@gmail.com>
2024-12-11 00:01:00 +0530

    other dbsets add

--- Sticker-web/Data/ApplicationDbContext.cs
+++ Sticker-web/Data/ApplicationDbContext.cs
@@ -16,12 +16,12 @@ namespace Sticker_web.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
-        //public DbSet<Vendor> Vendors { get; set; }
-        //public DbSet<Courier> Couriers { get; set; }
-        //public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Courier> Couriers { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
-        //public DbSet<OrderHeader> OrderHeaders { get; set; }
-        //public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
