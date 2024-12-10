b492489d5e972db4fefdd335626241de56128db1
Sowparna Shome <soupornoshome@gmail.com>
2024-12-10 22:11:30 +0530

    category service for unit of work consumption and error handling

---
+++ Sticker-web/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Sticker_web.Models;
+
+namespace Sticker_web.Services
+{
+    public interface ICategoryService
+    {
+        Task AddAsync(Category obj);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetAsync(int id);
+        Task RemoveAsync(Category obj);
+        Task UpdateAsync(Category obj);
+    }
+}
