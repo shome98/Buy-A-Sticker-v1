3e1fa296e5fcc89fd145237b4d770215319ec3e7
Sowparna Shome <soupornoshome@gmail.com>
2024-12-10 21:19:46 +0530

    web part add via project

---
+++ Sticker-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sticker_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
