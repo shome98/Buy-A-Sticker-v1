eb142d5b32c201ca4be470dcba4def8060eff2ee
Sowparna Shome <soupornoshome@gmail.com>
2024-12-10 22:09:36 +0530

    category service create

---
+++ Sticker-web/Repository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sticker_web.Models;
+
+namespace Sticker_web.Repository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        Task UpdateAsync(Category obj);
+    }
+}
