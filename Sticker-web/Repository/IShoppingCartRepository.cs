8fe40cad3c279ae508bd6402cc63f38c5df94acf
Sowparna Shome <soupornoshome@gmail.com>
2024-12-11 21:57:18 +0530

    shopping cart repository add

---
+++ Sticker-web/Repository/IShoppingCartRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sticker_web.Models;
+
+namespace Sticker_web.Repository
+{
+    public interface IShoppingCartRepository : IRepository<ShoppingCart>
+    {
+        Task UpdateAsync(ShoppingCart obj);
+    }
+}
---
+++ Sticker-web/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,20 @@
+﻿using Sticker_web.Data;
+using Sticker_web.Models;
+
+namespace Sticker_web.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task UpdateAsync(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
