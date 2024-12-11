d1eefbd3178dff097afd0bcff454d264cda91ebb
Sowparna Shome <soupornoshome@gmail.com>
2024-12-11 22:02:37 +0530

    vendor repository

---
+++ Sticker-web/Repository/IVendorRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sticker_web.Models;
+
+namespace Sticker_web.Repository
+{
+    public interface IVendorRepository : IRepository<Vendor>
+    {
+        Task UpdateAsync(Vendor obj);
+    }
+}
---
+++ Sticker-web/Repository/VendorRepository.cs
@@ -0,0 +1,20 @@
+﻿using Sticker_web.Data;
+using Sticker_web.Models;
+
+namespace Sticker_web.Repository
+{
+    public class VendorRepository : Repository<Vendor>, IVendorRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VendorRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task UpdateAsync(Vendor obj)
+        {
+            _db.Vendors.Update(obj);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
