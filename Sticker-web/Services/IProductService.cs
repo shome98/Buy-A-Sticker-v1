bd913993fd4da8aeb1a2553bdb731c19be62b5c4
Sowparna Shome <soupornoshome@gmail.com>
2024-12-11 22:09:50 +0530

    product service add

---
+++ Sticker-web/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Sticker_web.Models.ViewModels;
+using Sticker_web.Models;
+
+namespace Sticker_web.Services
+{
+    public interface IProductService
+    {
+        Task AddAsync(Product obj);
+        Task<IEnumerable<Product>> GetAllAsync(string include);
+        Task<Product> GetAsync(int? id);
+        ProductVM ImageHandle(ProductVM productVM, IFormFile? file);
+        Task RemoveAsync(Product obj);
+        Task UpdateAsync(Product obj);
+    }
+}
