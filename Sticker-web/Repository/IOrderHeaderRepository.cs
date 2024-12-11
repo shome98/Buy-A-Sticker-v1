e87af398c6299e3d127aed990f802f60cdc4ba0b
Sowparna Shome <soupornoshome@gmail.com>
2024-12-11 21:59:47 +0530

    order header repository

---
+++ Sticker-web/Repository/IOrderHeaderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Sticker_web.Models;
+
+namespace Sticker_web.Repository
+{
+    public interface IOrderHeaderRepository : IRepository<OrderHeader>
+    {
+        Task UpdateAsync(OrderHeader obj);
+        Task UpdateStatusAsync(int id, string orderStatus, string? paymentStatus = null);
+        Task UpdateStripePaymentIdAsync(int id, string sessionId, string paymentIntentId);
+    }
+}
