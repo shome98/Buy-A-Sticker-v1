7dd65b2a3f80496e292b8be82dae62a542ba8afb
Sowparna Shome <soupornoshome@gmail.com>
2024-12-11 21:52:42 +0530

    product repository add

---
+++ Sticker-web/Repository/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sticker_web.Data;
+using Sticker_web.Models;
+
+namespace Sticker_web.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task UpdateAsync(Product obj)
+        {
+            var objFromDb = await _db.Products.FirstOrDefaultAsync(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.Description = obj.Description;
+                objFromDb.Price = obj.Price;
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+            await _db.SaveChangesAsync();
+        }
+    }
+}
